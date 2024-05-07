53a9cc3e4277f8f9d29efbfc232e82c1b7d5dc4d
kawan.singh <kawan.singh@nagarro.com>
2024-05-07 22:34:58 +0530

    First Commit

---
+++ DevopsApi.Test/Testing.cs
@@ -0,0 +1,27 @@
+using DevOpsApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevopsApi.Test
+{
+    public class Testing
+    {
+        private WeatherForecastController controller;
+
+        public Testing(WeatherForecastController _controller)
+        {
+            this.controller = _controller;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+            //Act
+            var result = controller.Get() as ViewResult;
+
+            //Assert
+            Assert.NotNull(result);
+
+        }
+    }
+}
